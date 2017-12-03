8b9d487600337e17e16843bdcb78f9e28390dd5f
Lucas Courot <lucas@courot.com>
2017-12-03 12:32:30 +0100

    Should reserve in the same coach for the same reservation

--- tests/TicketOfficeTest.php
+++ tests/TicketOfficeTest.php
@@ -213,6 +213,55 @@ class TicketOfficeTest extends TestCase
         );
     }
 
+    public function testShouldReserveSeatsInTheSameCoachForTheSameReservation()
+    {
+        // Given
+        $this->trainDataProvider->fetchTrainTopology($this->trainId)->willReturn(new TrainTopology([
+            new Coach([
+                new ReservedSeat('A1', $this->bookingReference),
+                new ReservedSeat('A2', $this->bookingReference),
+                new ReservedSeat('A3', $this->bookingReference),
+                new ReservedSeat('A4', $this->bookingReference),
+                new ReservedSeat('A5', $this->bookingReference),
+                new ReservedSeat('A6', $this->bookingReference),
+                new AvailableSeat('A7'),
+                new AvailableSeat('A8'),
+                new AvailableSeat('A9'),
+                new AvailableSeat('A10'),
+            ]),
+            new Coach([
+                new ReservedSeat('B1', $this->bookingReference),
+                new ReservedSeat('B2', $this->bookingReference),
+                new ReservedSeat('B3', $this->bookingReference),
+                new ReservedSeat('B4', $this->bookingReference),
+                new ReservedSeat('B5', $this->bookingReference),
+                new AvailableSeat('B6'),
+                new AvailableSeat('B7'),
+                new AvailableSeat('B8'),
+                new AvailableSeat('B9'),
+                new AvailableSeat('B10'),
+            ]),
+        ]));
+
+        // Expect
+        $this->trainDataProvider->markSeatsAsReserved(Argument::any(), Argument::any())->shouldBeCalled();
+
+        // When
+        $ticketOffice = new TicketOffice($this->bookingReferenceProvider->reveal(), $this->trainDataProvider->reveal());
+        $reservationConfirmation = $ticketOffice->makeReservation(new ReservationRequest($this->trainId, 2));
+
+        // Then
+        $this->assertEquals($this->trainId, $reservationConfirmation->getTrainId());
+        $this->assertEquals($this->bookingReference, $reservationConfirmation->getBookingReference());
+        $this->assertEquals(
+            [
+                new ReservedSeat('B6', $this->bookingReference),
+                new ReservedSeat('B7', $this->bookingReference),
+            ],
+            $reservationConfirmation->getReservedSeats()
+        );
+    }
+
     private function trainWith1CoachAnd10AvailableSeats()
     {
         return new TrainTopology([
